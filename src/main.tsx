9c8f4270c2e0ecbdca18f09b609aa4fba534810e
dnhfan <dinhan.0321@gmail.com>
2026-01-21 08:58:15 +0700

    fix: full css

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./app.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
